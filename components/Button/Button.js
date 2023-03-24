1f762322792e94e0b22b8b66e572becdf74c9d22
ProcessX <luc2526@gmail.com>
2023-03-24 15:15:18 +0100

    Button Created

---
+++ components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Button.module.scss';
+
+function Button() {
+  return (
+    <a className={styles.Button}>Button</a>
+  )
+}
+
+export default Button
